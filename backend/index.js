7456a00dd0100d6268da16109071f8019772d595
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-21 08:53:14 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
+const path = require('path');
+
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -24,13 +27,31 @@ mongoose
   console.log("Connection failed!");
 });
 
-const path = require('path')
 app.use('/', express.static(path.join(__dirname, 'public')))
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
