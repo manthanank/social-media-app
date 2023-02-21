2f316f4fa859fcf50d99ba097f4b32acbb076b74
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-21 09:20:28 +0530

    updated

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
+const express = require('express');
+const router = express.Router();
 const User = require("../models/User");
-const router = require("express").Router();
 const bcrypt = require("bcrypt");
 
 //update user
